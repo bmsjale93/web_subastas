9403f148c198a4be9c9d0a9cd1265399b09974e5
bmsjale93 <117013312+bmsjale93@users.noreply.github.com>
2024-08-11 12:40:49 +0200

    Ajuste

--- subasta_detalle.php
+++ subasta_detalle.php
@@ -155,7 +155,7 @@ if (!is_numeric($latitud) || !is_numeric($altitud)) {
 
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="/config.api.js"></script>
+    <script src="/config_api.js"></script>
     <script type="module">
         import {
             renderCountdown
